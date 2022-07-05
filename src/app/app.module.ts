f9a68a35705aed7f29c4afa720956bad81b457e9
fickkmdev <fickkm.dev@gmail.com>
2022-07-05 11:05:20 +0700

    [fickkm] create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { PersonalComponent } from './components/personal/personal.component';
+import { BookingComponent } from './components/booking/booking.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { BookingPreviewComponent } from './components/booking-preview/booking-preview.component';
+import { LoginComponent } from './components/login/login.component'
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, HomeComponent, PersonalComponent, BookingComponent, NavBarComponent, BookingPreviewComponent, LoginComponent],
     imports: [BrowserModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
---
+++ src/app/components/booking-preview/booking-preview.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BookingPreviewComponent } from './booking-preview.component';
+
+describe('BookingPreviewComponent', () => {
+  let component: BookingPreviewComponent;
+  let fixture: ComponentFixture<BookingPreviewComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BookingPreviewComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BookingPreviewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
