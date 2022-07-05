8325e1437049261545782df1302710298aa47d70
pahnta <panta.s@odds.team>
2022-07-05 17:07:38 +0700

    [panta] add button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,10 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component'
 import { BookingPreviewComponent } from './components/booking-preview/booking-preview.component'
 import { SignInComponent } from './components/sign-in/sign-in.component'
 import { MentorCardComponent } from './components/mentor-card/mentor-card.component'
-
+import {InputTextModule} from 'primeng/inputtext';
 import { AccordionModule } from 'primeng/accordion'
 import { ButtonModule } from 'primeng/button'
+import { ChipModule } from 'primeng/chip';
 
 @NgModule({
     declarations: [
@@ -25,7 +26,7 @@ import { ButtonModule } from 'primeng/button'
         SignInComponent,
         MentorCardComponent,
     ],
-    imports: [BrowserModule, AppRoutingModule, AccordionModule, ButtonModule],
+    imports: [BrowserModule, AppRoutingModule, AccordionModule, ButtonModule,InputTextModule,ChipModule],
     providers: [],
     bootstrap: [AppComponent],
 })
