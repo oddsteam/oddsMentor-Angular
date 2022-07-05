3514e86ab00ddb2f4ceaddeb147b1f17fd69f01f
Phanuwat Phoowichai <phanu_phoo@kkumail.com>
2022-07-05 15:56:16 +0700

    [taliw] add home and fix ng v

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { BookingComponent } from './components/booking/booking.component'
 import { NavBarComponent } from './components/nav-bar/nav-bar.component'
 import { BookingPreviewComponent } from './components/booking-preview/booking-preview.component'
 import { SignInComponent } from './components/sign-in/sign-in.component'
+import { MentorCardComponent } from './components/mentor-card/mentor-card.component'
+
+import { AccordionModule } from 'primeng/accordion'
+import { ButtonModule } from 'primeng/button'
 
 @NgModule({
     declarations: [
@@ -19,8 +23,9 @@ import { SignInComponent } from './components/sign-in/sign-in.component'
         NavBarComponent,
         BookingPreviewComponent,
         SignInComponent,
+        MentorCardComponent,
     ],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule, AppRoutingModule, AccordionModule, ButtonModule],
     providers: [],
     bootstrap: [AppComponent],
 })
---
+++ src/app/components/mentor-card/mentor-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mentor-card',
+  templateUrl: './mentor-card.component.html',
+  styleUrls: ['./mentor-card.component.css']
+})
+export class MentorCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
