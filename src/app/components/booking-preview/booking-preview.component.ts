610553ac7b5b7ac501034bac982d5de8f3c63f83
Phanuwat Phoowichai <phanu_phoo@kkumail.com>
2022-07-07 11:52:36 +0700

    [taliw] rm discord noti

--- src/app/components/booking-preview/booking-preview.component.ts
+++ src/app/components/booking-preview/booking-preview.component.ts
@@ -41,79 +41,11 @@ export class BookingPreviewComponent implements OnInit {
 
     //alert
     async on_submit() {
-        this.send2Discord(this.bookingDetail)
+        // this.send2Discord(this.bookingDetail)
         await Swal.fire({
             icon: 'success',
             title: 'Thank you for your booking!',
         })
         this.router.navigateByUrl('personal')
-    }
-
-    send2Discord(booking: BookingDetail) {
-        let discordUrl =
-            'https://discord.com/api/webhooks/994429221705351338/VJx4TreAciMRFJUH6pG4BRvHXgmKNmaVL51xs8vy77YlLza-k1r3m4YN9bIbAdvF2CY8'
-        let body = {
-            content: null,
-            embeds: [
-                {
-                    title: 'ODDS Mentor',
-                    url: 'http://159.138.240.167:8089/',
-                    color: 1349845,
-                    fields: [
-                        {
-                            name: 'Name',
-                            value: booking.firstNameUser + ' ' + booking.lastNameUser,
-                        },
-                        {
-                            name: 'Email',
-                            value: booking.emailUser,
-                        },
-                        {
-                            name: 'Mentor name',
-                            value: booking.firstNameMentor + ' ' + booking.lastNameMentor,
-                        },
-                        {
-                            name: 'Expertises',
-                            value: this.expertise,
-                        },
-                        {
-                            name: 'Reason',
-                            value: booking.reason,
-                        },
-                        {
-                            name: 'Date and Time',
-                            value:
-                                [
-                                    booking.bookingDate.getDate(),
-                                    booking.bookingDate.getMonth() + 1,
-                                    booking.bookingDate.getFullYear(),
-                                ].join('/') +
-                                ' ' +
-                                booking.bookingTime.getHours() +
-                                ':' +
-                                booking.bookingTime.getMinutes(),
-                        },
-                        {
-                            name: 'Duration',
-                            value: booking.duration.time + ' mins',
-                        },
-                    ],
-                    footer: {
-                        text: 'from ODDS Mentor',
-                        icon_url:
-                            'https://cdn.discordapp.com/avatars/994429221705351338/e7a311c3d23bd94d1abcf22511a60089.png',
-                    },
-                    timestamp: new Date(),
-                },
-            ],
-        }
-
-        let headers = {
-            'Content-Type': 'application/json',
-        }
-
-        this.http.post<any>(discordUrl, body, { headers }).subscribe((data) => {
-            console.log(data)
-        })
     }
 }
