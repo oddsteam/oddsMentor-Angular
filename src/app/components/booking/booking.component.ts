a4ff746542625498729a0c65fb41034808a4f308
Phanuwat Phoowichai <phanu_phoo@kkumail.com>
2022-07-12 11:58:42 +0700

    [taliw] update booking form and time picker

--- src/app/components/booking/booking.component.ts
+++ src/app/components/booking/booking.component.ts
@@ -6,6 +6,10 @@ import { BookingsService } from 'src/app/services/bookings.service'
 import { MentorsService } from 'src/app/services/mentors.service'
 import { BookingDetail, MentorDetail } from 'src/app/mentor'
 
+interface Time {
+    value: string
+}
+
 @Component({
     selector: 'app-booking',
     templateUrl: './booking.component.html',
@@ -14,7 +18,298 @@ import { BookingDetail, MentorDetail } from 'src/app/mentor'
 export class BookingComponent implements OnInit {
     expertises: string[] = []
     selectedExpertises: string[] = []
-    duration: number[] = [30, 45, 60]
+    duration: string[] = ['15 mins', '30 mins', '45 mins', '1 hour']
+    timesArray: Time[] = [
+        {
+            value: '0:00',
+        },
+        {
+            value: '0:15',
+        },
+        {
+            value: '0:30',
+        },
+        {
+            value: '0:45',
+        },
+        {
+            value: '1:00',
+        },
+        {
+            value: '1:15',
+        },
+        {
+            value: '1:30',
+        },
+        {
+            value: '1:45',
+        },
+        {
+            value: '2:00',
+        },
+        {
+            value: '2:15',
+        },
+        {
+            value: '2:30',
+        },
+        {
+            value: '2:45',
+        },
+        {
+            value: '3:00',
+        },
+        {
+            value: '3:15',
+        },
+        {
+            value: '3:30',
+        },
+        {
+            value: '3:45',
+        },
+        {
+            value: '4:00',
+        },
+        {
+            value: '4:15',
+        },
+        {
+            value: '4:30',
+        },
+        {
+            value: '4:45',
+        },
+        {
+            value: '5:00',
+        },
+        {
+            value: '5:15',
+        },
+        {
+            value: '5:30',
+        },
+        {
+            value: '5:45',
+        },
+        {
+            value: '6:00',
+        },
+        {
+            value: '6:15',
+        },
+        {
+            value: '6:30',
+        },
+        {
+            value: '6:45',
+        },
+        {
+            value: '7:00',
+        },
+        {
+            value: '7:15',
+        },
+        {
+            value: '7:30',
+        },
+        {
+            value: '7:45',
+        },
+        {
+            value: '8:00',
+        },
+        {
+            value: '8:15',
+        },
+        {
+            value: '8:30',
+        },
+        {
+            value: '8:45',
+        },
+        {
+            value: '9:00',
+        },
+        {
+            value: '9:15',
+        },
+        {
+            value: '9:30',
+        },
+        {
+            value: '9:45',
+        },
+        {
+            value: '10:00',
+        },
+        {
+            value: '10:15',
+        },
+        {
+            value: '10:30',
+        },
+        {
+            value: '10:45',
+        },
+        {
+            value: '11:00',
+        },
+        {
+            value: '11:15',
+        },
+        {
+            value: '11:30',
+        },
+        {
+            value: '11:45',
+        },
+        {
+            value: '12:00',
+        },
+        {
+            value: '12:15',
+        },
+        {
+            value: '12:30',
+        },
+        {
+            value: '12:45',
+        },
+        {
+            value: '13:00',
+        },
+        {
+            value: '13:15',
+        },
+        {
+            value: '13:30',
+        },
+        {
+            value: '13:45',
+        },
+        {
+            value: '14:00',
+        },
+        {
+            value: '14:15',
+        },
+        {
+            value: '14:30',
+        },
+        {
+            value: '14:45',
+        },
+        {
+            value: '15:00',
+        },
+        {
+            value: '15:15',
+        },
+        {
+            value: '15:30',
+        },
+        {
+            value: '15:45',
+        },
+        {
+            value: '16:00',
+        },
+        {
+            value: '16:15',
+        },
+        {
+            value: '16:30',
+        },
+        {
+            value: '16:45',
+        },
+        {
+            value: '17:00',
+        },
+        {
+            value: '17:15',
+        },
+        {
+            value: '17:30',
+        },
+        {
+            value: '17:45',
+        },
+        {
+            value: '18:00',
+        },
+        {
+            value: '18:15',
+        },
+        {
+            value: '18:30',
+        },
+        {
+            value: '18:45',
+        },
+        {
+            value: '19:00',
+        },
+        {
+            value: '19:15',
+        },
+        {
+            value: '19:30',
+        },
+        {
+            value: '19:45',
+        },
+        {
+            value: '20:00',
+        },
+        {
+            value: '20:15',
+        },
+        {
+            value: '20:30',
+        },
+        {
+            value: '20:45',
+        },
+        {
+            value: '21:00',
+        },
+        {
+            value: '21:15',
+        },
+        {
+            value: '21:30',
+        },
+        {
+            value: '21:45',
+        },
+        {
+            value: '22:00',
+        },
+        {
+            value: '22:15',
+        },
+        {
+            value: '22:30',
+        },
+        {
+            value: '22:45',
+        },
+        {
+            value: '23:00',
+        },
+        {
+            value: '23:15',
+        },
+        {
+            value: '23:30',
+        },
+        {
+            value: '23:45',
+        },
+    ]
+    selectedTime!: Time
 
     bookingForm = new FormGroup(
         {
@@ -43,8 +338,39 @@ export class BookingComponent implements OnInit {
     ngOnInit(): void {
         // get mentor data
         const serviceData = this.mentorsService.getCurrentMentor()
-        if (!serviceData) this.router.navigateByUrl('')
-        this.mentorSelected = this.mentorsService.getCurrentMentor()!
+        // if (!serviceData) this.router.navigateByUrl('')
+        // this.mentorSelected = this.mentorsService.getCurrentMentor()!
+
+        if (!serviceData) {
+            this.mentorSelected = {
+                id: '62c70dedf9c68f9f4d00cf40',
+                fullNameEN: 'Chandara Sin',
+                fullNameTH: 'จันทร์ดารา ซิน',
+                nickname: 'โดม',
+                type: 'Cooperative Education 2022',
+                biography:
+                    "Hello, I'm Chandara Sin. I'm a software developer. I'm currently working at Odd-e (Thailand) in Mola Mola team.",
+                team: 'Molamola',
+                position: 'Software Developer',
+                profileImageUrl:
+                    'https://drive.google.com/uc?export=view&id=18SM8TbikJQt-RH2LdS_MbzGGod-dXHgx',
+                totalEndorsed: 0,
+                expertise: [
+                    {
+                        id: '62c5b2004743e83ab91f2a26',
+                        skill: 'JavaScript',
+                        endorsed: 0,
+                    },
+                    {
+                        id: '62c65c85020d0f6ecbdc58a5',
+                        skill: 'Java',
+                        endorsed: 0,
+                    },
+                ],
+            }
+        } else {
+            this.mentorSelected = this.mentorsService.getCurrentMentor()!
+        }
 
         this.mentorSelected.expertise.forEach((expertise) => {
             this.expertises.push(expertise.skill)
@@ -75,19 +401,20 @@ export class BookingComponent implements OnInit {
             //     expertise: [],
             //     reason: 'Developing a new product',
             //     sessionDate: '',
+            //     sessionTime: '',
             //     sessionDuration: 60,
             // })
 
             // Default Data
             this.bookingForm.setValue({
                 userId: '62c6ecc318a930d4ec607c39',
-                userFullName: '',
-                userEmail: '',
+                userFullName: 'Pjanuwat Phoowichai',
+                userEmail: 'taliw_phanxz@odds.team',
                 mentorId: this.mentorSelected.id,
                 mentorFullName: this.mentorSelected.fullNameEN,
                 expertise: [],
-                reason: '',
-                sessionDate: '',
+                reason: null,
+                sessionDate: null,
                 sessionDuration: null,
             })
         }
