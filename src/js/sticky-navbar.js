0a48c728e2929dd038e44d22b1f05a26dfd7cad1
himenoworks <himeno.works001@gmail.com>
2022-07-18 16:16:21 +0700

    [nu] edit home page and edit card style

---
+++ src/js/sticky-navbar.js
@@ -0,0 +1,7 @@
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+        $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+    } else {
+        $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+    }
+});
