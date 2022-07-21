c5b1b0eb231975d5eb9601e8147e7104e08a8585
phanu-phoo <phanu_phoo@kkumail.com>
2022-07-21 21:39:00 +0700

    [taliw] test add expertise to fit box

--- src/app/components/mentor-card/mentor-card.component.ts
+++ src/app/components/mentor-card/mentor-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core'
+import { Component, Input, OnInit, AfterViewInit } from '@angular/core'
+import { flush } from '@angular/core/testing'
 import { Router } from '@angular/router'
 import { MentorDetail } from 'src/app/mentor'
 import { MentorsService } from 'src/app/services/mentors.service'
@@ -8,26 +9,71 @@ import { MentorsService } from 'src/app/services/mentors.service'
     templateUrl: './mentor-card.component.html',
     styleUrls: ['./mentor-card.component.css'],
 })
-export class MentorCardComponent implements OnInit {
+export class MentorCardComponent implements OnInit, AfterViewInit {
     @Input()
     mentorDetail!: MentorDetail
     skills: string[] = []
+    dummyXpertise: string[] = []
 
     constructor(private router: Router, private mentorsService: MentorsService) {}
 
     ngOnInit(): void {
         let expertise = this.mentorDetail.expertise
         expertise.sort((first, second) => second.endorsed - first.endorsed)
-        if (expertise.length > 4) {
-            for (let index = 0; index < 3; index++) {
-                this.skills.push(expertise[index].skill)
+        expertise.forEach((expertise) => {
+            this.dummyXpertise.push(expertise.skill)
+        })
+        // if (expertise.length > 4) {
+        //     for (let index = 0; index < 3; index++) {
+        //         this.skills.push(expertise[index].skill)
+        //     }
+        //     this.skills.push(`+${expertise.length - 3} more`)
+        // } else {
+        //     expertise.forEach((expertise) => {
+        //         this.skills.push(expertise.skill)
+        //     })
+        // }
+    }
+
+    ngAfterViewInit(): void {
+        let LINE_WIDTH = 248
+        let expertiseChip = window.document.getElementById(`dummyChip-{{ mentorDetail.id }}`)
+        let haveNextLine = true
+        let lineWidth = LINE_WIDTH
+        let skillCal: string[] = []
+
+        for (const x of this.dummyXpertise) {
+            let newChip = this.createSkillChip(x)
+            expertiseChip?.appendChild(newChip)
+            let chipWidth = newChip.offsetWidth
+            // console.log(chipWidth);
+            if (chipWidth < lineWidth - 6) {
+                skillCal.push(newChip.innerText)
+                lineWidth -= chipWidth + 6
+                // console.log(`${x} width: ${chipWidth} => ${lineWidth}`);
+            } else if (haveNextLine) {
+                haveNextLine = false
+                // console.log('Set haveNextLine to false');
+                lineWidth = LINE_WIDTH
+                skillCal.push(newChip.innerText)
+                lineWidth -= chipWidth + 82
+            } else {
+                // console.log('OVERFLOW!')
+                break
             }
-            this.skills.push(`+${expertise.length - 3} more`)
-        } else {
-            expertise.forEach((expertise) => {
-                this.skills.push(expertise.skill)
-            })
         }
+
+        this.skills = skillCal
+        this.skills.push(`+${this.dummyXpertise.length - skillCal.length} more`)
+        expertiseChip?.setAttribute('hidden', '')
+    }
+
+    createSkillChip(skillName: string): HTMLElement {
+        let newChip = document.createElement('span')
+        newChip.setAttribute('_ngcontent-serverapp-c63', '')
+        newChip.className = 'badge bg-expertise'
+        newChip.innerHTML = skillName
+        return newChip
     }
 
     onMentor() {
